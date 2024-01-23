bd0f1743a1304c5cf6712184c1b6b7c634100303
Diego de Massy <67842833+DiegoMarck@users.noreply.github.com>
2024-01-23 20:54:53 +0100

    modifications pour affichage route /cameras

--- src/Controller/CameraController.php
+++ src/Controller/CameraController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/camera")
+ * @Route("/cameras")
  */
 class CameraController extends AbstractController
 {
